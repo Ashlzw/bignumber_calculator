b30ef36e927cd1d8d95437605e62a40a6c6cb38c
lzw <leeashe@outlook.com>
2017-03-21 23:54:30 +0800

    Update test.cpp
    basically test for add, sub, mul and div

--- src/test.cpp
+++ src/test.cpp
@@ -4,22 +4,6 @@ static int main_ret = 0;
 static int test_count = 0;
 static int test_pass = 0;
 
-std::string data_to_str(bignumber &bn)
-{
-	int length = bn._len;
-	std::string str = std::string(length, 0);
-
-	for (size_t i = 0; i < length; i++)
-	{
-		str[i] = bn._data[length - i - 1] + '0';
-	}
-	bn.trim_str(str);
-	if (bn._sign == 1 && bn._len > 1)
-		str.insert(str.begin(), '-');
-
-	return str;
-}
-
 #define EXPECT_EQ_BASE(equality, expect, actual) \
 	do\
 	{\
@@ -39,8 +23,24 @@ std::string data_to_str(bignumber &bn)
 	do\
 	{\
 		bignumber bnum(actual);\
-		EXPECT_EQ_INTEGER(expect, data_to_str(bnum));\
+		EXPECT_EQ_INTEGER(expect, bnum.to_string());\
 	} while (0)
+
+#define TEST_OPERATOR(expect, input1, input2, op)\
+	do\
+	{\
+		bignumber a(input1);\
+		bignumber b(input2);\
+		TEST_BIGNUMBER(expect, a op b);\
+	} while (0)
+
+#define TEST_ADD(expect, input1, input2) TEST_OPERATOR(expect, input1, input2, +)
+
+#define TEST_SUB(expect, input1, input2) TEST_OPERATOR(expect, input1, input2, -)
+
+#define TEST_MUL(expect, input1, input2) TEST_OPERATOR(expect, input1, input2, *)
+
+#define TEST_DIV(expect, input1, input2) TEST_OPERATOR(expect, input1, input2, /)
 
 static void test_bignumber_construct()
 {
@@ -53,9 +53,85 @@ static void test_bignumber_construct()
 	TEST_BIGNUMBER(std::string("-123"), std::string("-123"));
 }
 
+static void test_bignumber_add()
+{
+	TEST_ADD(std::string("0"), std::string("0"), std::string("0"));
+	TEST_ADD(std::string("0"), std::string("-0"), std::string("-0"));
+	TEST_ADD(std::string("1"), std::string("0"), std::string("1"));
+	TEST_ADD(std::string("-1"), std::string("0"), std::string("-1"));
+	TEST_ADD(std::string("100"), std::string("99"), std::string("1"));
+	TEST_ADD(std::string("99"), std::string("100"), std::string("-1"));
+	TEST_ADD(std::string("1070553071"), std::string("85894615"), std::string("984658456"));
+	TEST_ADD(std::string("85894614"), std::string("85894615"), std::string("-1"));
+	TEST_ADD(std::string("22222222222222"), std::string("22222222222222"), std::string("0"));
+	TEST_ADD(std::string("22222222222222"), std::string("11111111111111"), std::string("11111111111111"));
+	TEST_ADD(std::string("-22222222222222"), std::string("-11111111111111"), std::string("-11111111111111"));
+	TEST_ADD(std::string("11111111111111"), std::string("22222222222222"), std::string("-11111111111111"));
+	TEST_ADD(std::string("10000000000000"), std::string("9999999999999"), std::string("1"));
+	TEST_ADD(std::string("-10000000000000"), std::string("-9999999999999"), std::string("-1"));
+}
+
+static void test_bignumber_sub()
+{
+	TEST_SUB(std::string("0"), std::string("0"), std::string("0"));
+	TEST_SUB(std::string("1"), std::string("1"), std::string("0"));
+	TEST_SUB(std::string("0"), std::string("1"), std::string("1"));
+	TEST_SUB(std::string("0"), std::string("-1"), std::string("-1"));
+	TEST_SUB(std::string("100"), std::string("101"), std::string("1"));
+	TEST_SUB(std::string("102"), std::string("101"), std::string("-1"));
+	TEST_SUB(std::string("99"), std::string("100"), std::string("1"));
+	TEST_SUB(std::string("-101"), std::string("-100"), std::string("1"));
+	TEST_SUB(std::string("85894615"), std::string("1070553071"),  std::string("984658456"));
+	TEST_SUB(std::string("-85894615"), std::string("-1070553071"), std::string("-984658456"));
+	TEST_SUB(std::string("22222222222222"), std::string("22222222222222"), std::string("0"));
+	TEST_SUB(std::string("-22222222222222"), std::string("-22222222222222"), std::string("0"));
+	TEST_SUB(std::string("11111111111111"), std::string("22222222222222"), std::string("11111111111111"));
+	TEST_SUB(std::string("-33333333333333"), std::string("-22222222222222"), std::string("11111111111111"));
+	TEST_SUB(std::string("-10000000000000"), std::string("0"), std::string("10000000000000"));
+}
+
+static void test_bignumber_mul()
+{
+	TEST_MUL(std::string("0"), std::string("0"), std::string("0"));
+	TEST_MUL(std::string("0"), std::string("1"), std::string("0"));
+	TEST_MUL(std::string("1"), std::string("1"), std::string("1"));
+	TEST_MUL(std::string("1"), std::string("-1"), std::string("-1"));
+	TEST_MUL(std::string("-1"), std::string("1"), std::string("-1"));
+	TEST_MUL(std::string("101"), std::string("101"), std::string("1"));
+	TEST_MUL(std::string("-101"), std::string("101"), std::string("-1"));
+	TEST_MUL(std::string("1000"), std::string("40"), std::string("25"));
+	TEST_MUL(std::string("85284543725746406280"), std::string("98646549864"), std::string("864546645"));
+	TEST_MUL(std::string("-85284543725746406280"), std::string("98646549864"), std::string("-864546645"));
+	TEST_MUL(std::string("0"), std::string("22222222222222"), std::string("0"));
+	TEST_MUL(std::string("-2222222222222222222222222222222222222222222222222222"), std::string("-2222222222222222222222222222222222222222222222222222"), std::string("1"));
+	TEST_MUL(std::string("99999999999999"), std::string("9"), std::string("11111111111111"));
+}
+
+static void test_bignumber_div()
+{
+	TEST_DIV(std::string("1"), std::string("1"), std::string("1"));
+	TEST_DIV(std::string("3"), std::string("3"), std::string("1"));
+	TEST_DIV(std::string("33"), std::string("33"), std::string("1"));
+	TEST_DIV(std::string("2049"), std::string("2049"), std::string("1"));
+	TEST_DIV(std::string("1"), std::string("-1"), std::string("-1"));
+	TEST_DIV(std::string("-1"), std::string("1"), std::string("-1"));
+	TEST_DIV(std::string("101"), std::string("101"), std::string("1"));
+	TEST_DIV(std::string("-101"), std::string("101"), std::string("-1"));
+	TEST_DIV(std::string("40"), std::string("1000"), std::string("25"));
+/*	TEST_DIV(std::string("98646549864"), std::string("85284543725746406280"), std::string("864546645"));
+	TEST_DIV(std::string("98646549864"), std::string("-85284543725746406280"), std::string("-864546645"));
+	TEST_DIV(std::string("-2222222222222222222222222222222222222222222222222222"), std::string("-2222222222222222222222222222222222222222222222222222"), std::string("1"));
+	TEST_DIV(std::string("9"), std::string("99999999999999"), std::string("11111111111111"));
+*/
+}
+
 int main()
 {
 	test_bignumber_construct();
+	test_bignumber_add();
+	test_bignumber_sub();
+	test_bignumber_mul();
+	test_bignumber_div();
 
 	std::cout.precision(8);
 	std::cout << test_pass << "/" << test_count << " (" << test_pass * 100.0 / test_count << "%) passed" << std::endl;
