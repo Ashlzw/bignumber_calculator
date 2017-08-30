cac4081aa0db8ab9435c9dcdb92cac37f3cd220b
lzw <leeashe@outlook.com>
2017-08-30 20:45:44 +0800

    Update bignumber.h
    1、更新运算符操作参数为 const 
    2、修改部分函数的成员属性
    3、增加对成员数据的访问函数和修改函数
    4、增加无符号对比函数

--- src/Header Files/bignumber.h
+++ src/Header Files/bignumber.h
@@ -11,40 +11,43 @@
 
 class bignumber
 {
-	friend std::ostream &operator<<(std::ostream &os, bignumber &bn);
-	friend bignumber operator+(bignumber &a, bignumber &b);
-	friend bignumber operator-(bignumber &a, bignumber &b);
-	friend bignumber operator*(bignumber &a, bignumber &b);
-	friend bignumber operator/(bignumber &a, bignumber &b);
-
+	friend std::ostream &operator<<(std::ostream &os, bignumber &bn);	
+	friend bignumber operator+(const bignumber &a, const bignumber &b);
+	friend bignumber operator-(const bignumber &a, const bignumber &b);
+	friend bignumber operator*(const bignumber &a, const bignumber &b);
+	friend bignumber operator/(const bignumber &a, const bignumber &b);
 public:
 	bignumber(int len, char sign);
 	bignumber(std::string &str);
 	bignumber(const bignumber &bn);
 	~bignumber();
 
-	std::string to_string();
-	int valid_len();
-
 	bignumber &operator=(const bignumber &a);
 
+	void set_sign(int sign) { _sign = sign; }
+
+	int length() { return _len; }
+	std::string to_string();
+	
 private:
 	char _sign;
 	int _len;
 	char *_data;
 
-	void init(int len, char sign) { _sign = sign; _len = len; _data = new char[len](); }
+	int valid_len();
+	
+	void init(int len, char sign) { _sign = sign; _len = len; _data = new char[len] {0}; }
 	void copy_from(char * data) { for (size_t i = 0; i < _len; i++) _data[i] = data[i]; }
 	void parse_bignumber(std::string &str);
 	void fill_data_from_str(const std::string &str);
-	void trim_str(std::string &str);
-	void plus_one();
-
+	
+	static void trim_str(std::string &str);
 	static void add_impl(const bignumber & a, const bignumber & b, bignumber &r);
 	static void sub_impl(const bignumber & a, const bignumber & b, bignumber &r);
 	static void mul_impl(const bignumber & a, const bignumber & b, bignumber &r);
-
-	static int cmp(bignumber & a, bignumber & b);
+	
+	static int cmp(const bignumber & a, const bignumber & b);
+	static int no_sign_cmp(const bignumber & a, const bignumber & b);
 };
 
 #endif // !BIGNUMBER_H
